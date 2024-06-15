53c74626e83ead2c2c524f05fe7a6376104def30
Ic3Brain <sivignonalexandre@gmail.com>
2024-06-15 18:11:40 +0200

    Animation Player
    avancer, reculer, saut, gauche, droite

---
+++ Coin Challenge/Assets/Script/AnimPlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimPlayerController : MonoBehaviour
+{
+    Animator PlayerAnimator;
+    
+    void Awake() 
+    {
+        PlayerAnimator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animation du saut
+        PlayerAnimator.SetFloat("Jump", Input.GetAxis("Jump"));
+
+        //animation pour avancer 
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            PlayerAnimator.SetBool("walk", true);
+        }
+        else if(Input.GetAxis("Vertical") == 0)
+        {
+            PlayerAnimator.SetBool("walk", false);
+        }
+
+        //animation reculer
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            PlayerAnimator.SetBool("BackWard", true);
+        }
+        else if(Input.GetAxis("Vertical") == 0)
+        {
+            PlayerAnimator.SetBool("BackWard", false);
+        }
+
+        //animation aller a droite
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            PlayerAnimator.SetBool("StrafeRight", true);
+        }
+        else if(Input.GetAxis("Horizontal") == 0)
+        {
+            PlayerAnimator.SetBool("StrafeRight", false);
+        }
+
+        //animation aller a gauche
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            PlayerAnimator.SetBool("StrafeLeft", true);
+        }
+        else if(Input.GetAxis("Horizontal") == 0)
+        {
+            PlayerAnimator.SetBool("StrafeLeft", false);
+        }
+    }
+}
+
+
